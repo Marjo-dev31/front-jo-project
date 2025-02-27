5b9c9a14b7c5c546c2e579853f6acb40e82c5535
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-02-27 17:31:46 +0100

    feat: navbar done

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,20 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [],
+    imports: [RouterLink, NgStyle],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    isAdmin = false;
+    isConnected = false;
+    isShow = false;
+
+    showSideMenu() {
+        this.isShow = !this.isShow;
+        console.log(this.isShow);
+    }
+}
