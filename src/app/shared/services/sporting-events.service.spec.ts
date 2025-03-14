f69cf195d0455f3f80df581b1ecc26c7c9e0fdd2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-14 15:09:04 +0100

    feat: add sportingeventsservice in homecomponent

---
+++ src/app/shared/services/sporting-events.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SportingEventsService } from './sporting-events.service';
+
+describe('SportingEventsService', () => {
+  let service: SportingEventsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SportingEventsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
