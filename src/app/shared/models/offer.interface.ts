dd6468fea5e8a1cc9577f5fd387a6a1b1eccbb77
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 15:15:57 +0100

    fix: add offer and event interface

---
+++ src/app/shared/models/offer.interface.ts
@@ -0,0 +1,8 @@
+export interface OfferInterface {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    numberOfSales: number;
+    imgUrl: string;
+}
--- src/app/shared/services/offer.service.ts
+++ src/app/shared/services/offer.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { OfferInterface } from '../models/offer.interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class OfferService {
-    offers = [
+    offers: OfferInterface[] = [
         {
             id: '1',
             title: 'solo',
