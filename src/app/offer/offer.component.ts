dfdb7cb294f5809ec8e79940e8f8b1eb91332c63
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 11:25:40 +0100

    feat: add formgroup and onsubmit form

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,11 +1,17 @@
 import { CurrencyPipe, NgClass, TitleCasePipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 import { OfferService } from '../shared/services/offer.service';
 import { sportingEventService } from '../shared/services/sporting-event.service';
 
 @Component({
     selector: 'app-offer',
-    imports: [CurrencyPipe, TitleCasePipe, NgClass],
+    imports: [CurrencyPipe, TitleCasePipe, NgClass, ReactiveFormsModule],
     templateUrl: './offer.component.html',
     styleUrl: './offer.component.css',
 })
@@ -13,10 +19,34 @@ export class OfferComponent {
     private readonly offerService = inject(OfferService);
     private readonly sportingEventService = inject(sportingEventService);
 
-    offers = signal(this.offerService.offers);
-    sportingEvents = signal(this.sportingEventService.events);
+    public offers = signal(this.offerService.offers);
+    public sportingEvents = signal(this.sportingEventService.events);
+
+    readonly form = new FormGroup({
+        offer: new FormControl('', [Validators.required]),
+        event: new FormControl('', [Validators.required]),
+        quantity: new FormControl('', [Validators.required]),
+    });
+
+    get offer() {
+        return this.form.get('offer') as FormControl;
+    }
+
+    get event() {
+        return this.form.get('event') as FormControl;
+    }
+
+    get quantity() {
+        return this.form.get('quantity') as FormControl;
+    }
 
     onSubmit() {
         console.log('toto');
+        const reservation = {
+            offer: this.offer.value,
+            event: this.event.value,
+            quantity: this.quantity.value,
+        };
+        console.log(reservation);
     }
 }
