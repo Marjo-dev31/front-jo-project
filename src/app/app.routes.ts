bcc640924e9109ec96e502a3a51904595dab2f3a
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 12:02:10 +0100

    feat: add gtc component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { GtcComponent } from './gtc/gtc.component';
 
 export const routes: Routes = [
     { path: 'politiquedeconfidentialite', component: PrivacyPolicyComponent },
+    { path: 'conditionsgeneralesdevente', component: GtcComponent },
     { path: '', component: HomeComponent },
 ];
