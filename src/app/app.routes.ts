5a4cfe8a51368f07007a213813920554c6fcdf3c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 12:13:24 +0100

    feat: add error component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { GtcComponent } from './gtc/gtc.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     { path: 'politiquedeconfidentialite', component: PrivacyPolicyComponent },
     { path: 'conditionsgeneralesdevente', component: GtcComponent },
+    { path: 'erreur', component: ErrorComponent },
     { path: '', component: HomeComponent },
+    { path: '**', component: HomeComponent },
 ];
