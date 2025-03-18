40ade94c8784ab36e85e096191084b63bc8fa7e3
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 11:41:59 +0100

    feat; privacy policy component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+    { path: 'politiquedeconfidentialite', component: PrivacyPolicyComponent },
+    { path: '', component: HomeComponent },
+];
