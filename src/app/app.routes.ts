c3b4dba0bd62f85a79a53ddbadf93b8a5d236179
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-18 18:14:04 +0100

    feat: add cart component and table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
 import { GtcComponent } from './gtc/gtc.component';
 import { ErrorComponent } from './error/error.component';
 import { OfferComponent } from './offer/offer.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
+    { path: 'panier', component: CartComponent },
+    { path: 'offers', component: OfferComponent },
     { path: 'politiquedeconfidentialite', component: PrivacyPolicyComponent },
     { path: 'conditionsgeneralesdevente', component: GtcComponent },
     { path: 'erreur', component: ErrorComponent },
     { path: '', component: HomeComponent },
-    { path: 'offers', component: OfferComponent },
     { path: '**', component: HomeComponent },
-    { path: 'offers', component: OfferComponent },
 ];
-
