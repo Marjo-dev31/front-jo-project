77d4257dde7c206c0cbe56c53e8a747dbe0e5b92
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-21 16:43:02 +0100

    feat: add graph component

--- src/app/admin/graph/graph.component.ts
+++ src/app/admin/graph/graph.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { OfferService } from '../../shared/services/offer.service';
 import { OfferInterface } from '../../shared/models/offer.interface';
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
     selector: 'app-graph',
@@ -8,8 +10,48 @@ import { OfferInterface } from '../../shared/models/offer.interface';
     templateUrl: './graph.component.html',
     styleUrl: './graph.component.css',
 })
-export class GraphComponent {
+export class GraphComponent implements OnInit {
     private readonly OfferService = inject(OfferService);
 
     offers = signal<OfferInterface[]>(this.OfferService.offers);
+    offersTitle: string[] = [];
+    numberOfSales: number[] = [];
+    chart!: Chart;
+
+    config: ChartConfiguration = {
+        type: 'bar',
+        data: {
+            labels: this.offersTitle,
+            datasets: [
+                {
+                    label: 'Nombre de ventes par offres',
+                    data: [10, 12, 1],
+                    backgroundColor: '#3d5a80',
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    };
+
+    ngOnInit() {
+        this.chart = new Chart('myChart', this.config);
+        this.getTitle();
+        this.getNumberOfSales();
+    }
+
+    getTitle() {
+        this.offers().forEach((el) => this.offersTitle.push(el.title));
+    }
+
+    getNumberOfSales() {
+        this.offers().forEach((el) =>
+            this.numberOfSales.push(el.numberOfSales),
+        );
+    }
 }
