57c48971e2d7475884f2fb7d3fe29bbb016c5ef8
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-26 12:10:37 +0100

    feat: add sidenav and routeroutlet

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
-import { EventAdminComponent } from './events/event-admin.component';
-import { OfferAdminComponent } from './offers/offers-admin.component';
-import { GraphComponent } from './graph/graph.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-admin',
-    imports: [EventAdminComponent, OfferAdminComponent, GraphComponent],
+    imports: [RouterOutlet, RouterLink],
     template: `<h1 class="text-4xl font-coustard my-10 text-center">
             Bienvenue sur votre espace administrateur
         </h1>
-        <app-offer-admin></app-offer-admin>
-        <app-event-admin></app-event-admin>
-        <app-graph></app-graph> `,
-    styles: '',
+        <div
+            class="w-64 h-1/2 bg-primary rounded-tr-lg text-secondary absolute"
+        >
+            <h2 class="text-center text-2xl font-bold font-coustard my-4">
+                Bonjour
+            </h2>
+            <ul class="">
+                <li class="">
+                    <a
+                        [routerLink]="['/backoffice/offers']"
+                        class="block text-xl p-4 hover:bg-gray-100 rounded-l-xl"
+                        >Offres</a
+                    >
+                </li>
+                <li>
+                    <a
+                        [routerLink]="['/backoffice/sportingevents']"
+                        class="block text-xl p-4 hover:bg-gray-100 rounded-l-xl"
+                        >Epreuves</a
+                    >
+                </li>
+                <li>
+                    <a
+                        [routerLink]="['/backoffice/sales']"
+                        class="block text-xl p-4 hover:bg-gray-100 rounded-l-xl"
+                        >Ventes</a
+                    >
+                </li>
+            </ul>
+        </div>
+        <router-outlet></router-outlet> `,
+    styles: ``,
 })
 export class AdminComponent {}
