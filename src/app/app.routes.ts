02c0cbcb558ed0e7393446f53384af7f802d4d02
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-26 12:10:37 +0100

    feat: add sidenav and routeroutlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,22 @@ import { ErrorComponent } from './shared/components/error/error.component';
 import { OfferComponent } from './offer/offer.component';
 import { CartComponent } from './cart/cart.component';
 import { AdminComponent } from './admin/admin.component';
+import { OfferAdminComponent } from './admin/offers/offers-admin.component';
+import { EventAdminComponent } from './admin/events/event-admin.component';
+import { GraphComponent } from './admin/graph/graph.component';
 
 export const routes: Routes = [
     { path: 'panier', component: CartComponent },
     { path: 'offers', component: OfferComponent },
-    { path: 'backoffice', component: AdminComponent },
+    {
+        path: 'backoffice',
+        component: AdminComponent,
+        children: [
+            { path: 'offers', component: OfferAdminComponent },
+            { path: 'sportingevents', component: EventAdminComponent },
+            { path: 'sales', component: GraphComponent },
+        ],
+    },
     { path: 'politiquedeconfidentialite', component: PrivacyPolicyComponent },
     { path: 'conditionsgeneralesdevente', component: GtcComponent },
     { path: 'erreur', component: ErrorComponent },
