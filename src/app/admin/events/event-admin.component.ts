a118b648bffd7040f1c7613e0a4dfd1a93bfde22
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-26 14:12:23 +0100

    fat: add onsubmit sportingevent form

--- src/app/admin/events/event-admin.component.ts
+++ src/app/admin/events/event-admin.component.ts
@@ -2,7 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { sportingEventService } from '../../shared/services/sporting-event.service';
 import { NgStyle, TitleCasePipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { SportingEventInterface } from '../../shared/models/sportingevent.interface';
+import {
+    SportingEventCreateInterface,
+    SportingEventInterface,
+} from '../../shared/models/sportingevent.interface';
 import {
     FormControl,
     FormGroup,
@@ -43,9 +46,38 @@ export class EventAdminComponent {
         });
     }
 
-    // a faire
     onSubmit() {
-        console.log('toto', this.addForm.value);
+        // create formdata to post img type file
+        const formData = new FormData();
+        formData.append('file', this.selectedFile as Blob);
+        this.sportingEventService.addImage(formData).subscribe();
+
+        // take the name of file and record in newSportingEvent
+        const imgUrl = this.imgUrl.value.split('\\').slice(-1);
+
+        const newSportingEvent: SportingEventCreateInterface = {
+            title: this.title.value,
+            description: this.description.value,
+            imgUrl: imgUrl[0],
+        };
+
+        // check if alreadyExist and make put or post
+        const sportingEventAlreadyExist = this.sportingEvents()?.find(
+            (el) => el.title === newSportingEvent.title,
+        );
+        if (sportingEventAlreadyExist) {
+            const updatedSportingEvent: SportingEventInterface = {
+                id: sportingEventAlreadyExist.id,
+                ...newSportingEvent,
+            };
+            this.sportingEventService
+                .updateSportingEvent(updatedSportingEvent)
+                .subscribe();
+        } else {
+            this.sportingEventService
+                .addSportingEvent(newSportingEvent)
+                .subscribe();
+        }
     }
 
     onFileSelected(event: Event) {
