4e17410a15da101b3d285f911e1718b86a9bdbc4
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-31 17:37:06 +0200

    feat: account component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,12 +11,14 @@ import { EventAdminComponent } from './admin/events/event-admin.component';
 import { GraphComponent } from './admin/graph/graph.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AccountComponent } from './account/account.component';
 
 export const routes: Routes = [
     { path: 'panier', component: CartComponent },
     { path: 'offers', component: OfferComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'espacepersonnel', component: AccountComponent },
     {
         path: 'backoffice',
         component: AdminComponent,
