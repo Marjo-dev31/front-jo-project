1ed504718e405abce7196518c67c60800bfdd411
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-03-31 15:41:45 +0200

    feat: add reactiveform in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-login',
-    imports: [RouterLink],
+    imports: [RouterLink, ReactiveFormsModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm = new FormGroup({
+        userName: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+    });
+
+    onSubmit() {
+        console.log(this.loginForm.value);
+    }
+
+    get userName() {
+        return this.loginForm.get('userName') as FormControl;
+    }
+    get password() {
+        return this.loginForm.get('password') as FormControl;
+    }
+}
