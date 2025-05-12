de33ef967682d57b2997496e77fcd59852a6342f
Marjory.BRAVO@fittingbox.com <marjory.bravo@fittingbox.com>
2025-05-12 17:14:08 +0200

    feat(jest): gtc, provacy, error, footer header ok

--- src/app/admin/offers/offers-admin.component.spec.ts
+++ src/app/admin/offers/offers-admin.component.spec.ts
@@ -1,23 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { OffersComponent } from './offers-admin.component';
+import { OfferAdminComponent } from './offers-admin.component';
 
 describe('OffersComponent', () => {
-  let component: OffersComponent;
-  let fixture: ComponentFixture<OffersComponent>;
+    let component: OfferAdminComponent;
+    let fixture: ComponentFixture<OfferAdminComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [OffersComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [OfferAdminComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(OffersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(OfferAdminComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
