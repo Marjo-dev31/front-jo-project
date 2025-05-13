cf2acb8190ee64e3c05beeca525d2d6ffaa9ef15
Marjory.BRAVO@fittingbox.com <marjory.bravo@fittingbox.com>
2025-05-13 17:53:32 +0200

    feat(jest): graph component done

--- src/app/admin/graph/graph.component.spec.ts
+++ src/app/admin/graph/graph.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphComponent } from './graph.component';
+import { OfferService } from '../../shared/services/offer.service';
+import { of } from 'rxjs';
 
 describe('GraphComponent', () => {
     let component: GraphComponent;
     let fixture: ComponentFixture<GraphComponent>;
 
+    const mockOfferService = {
+        getAllOffers: jest.fn().mockReturnValue(of([{}])),
+    };
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [GraphComponent],
+            providers: [{ provide: OfferService, useValue: mockOfferService }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(GraphComponent);
